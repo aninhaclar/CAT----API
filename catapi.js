e894bed67e1ecccea0bbda2c0ae64829b41b02f8
ANA CLARA RIMUALDO PIRES <24122542@CFP127ALUNO.LOCAL>
2025-02-18 17:25:13 -0300

    Rever os códigos e ajustar as imagens

---
+++ catapi.js
@@ -0,0 +1,26 @@
+'use strict'
+
+async function pesquisarFotos(){
+    const url = `https://api.thecatapi.com/v1/images/search?limit=20` 
+
+    const response  = await fetch(url) 
+    const data      = await response.json() 
+    return data.map(cat => cat.url)
+}
+
+
+function criarImagem(link){ 
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img') 
+    novaImg.src = link 
+
+    galeria.appendChild(novaImg)
+}
+
+async function preencherFotos(){
+    const fotos = await pesquisarFotos()
+
+    fotos.forEach (criarImagem)
+}
+
+preencherFotos()
